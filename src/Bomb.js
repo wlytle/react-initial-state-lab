fcd6235d93432d5c76b945fbc7bfa98776390751
wlytle <wlytle89@gmail.com>
2020-12-10 11:19:15 -0700

    commit tree dfda08b11165a69887de5aec772886cc8ec46177

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+class Bomb extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  timerDispaly = () => {
+    return this.state.secondsLeft === 0
+      ? `Boom!`
+      : `${this.state.secondsLeft} seconds left before I go boom!`;
+  };
+
+  render() {
+    return <div>{this.timerDispaly()}</div>;
+  }
+}
+
+export default Bomb;
